d7668668a0ca9d5a2cc630f5645719bc879c3dc1
unknown <daniellourencoedu@gmail.com>
2023-05-03 18:01:15 +0100

    current year update fn added

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,12 +2,13 @@ import { BackTop } from "../";
 import "./Footer.sass";
 
 export const Footer = () => {
+	const currentYear: number = new Date().getFullYear();
 	return (
 		<div className="main-container footer-main-container">
 			<BackTop />
 			<section className="footer center">
 				<p className="text">
-					COPYRIGHT &copy; 2017 FUTURE. ALL RIGHTS RESERVED
+					COPYRIGHT &copy; {currentYear} FUTURE. ALL RIGHTS RESERVED
 				</p>
 				<p className="text">FACEBOOK | TWITTER | GOOGLE+ | LINKEDIN</p>
 			</section>
